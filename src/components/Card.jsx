9a54f1e010f1571301c0569e1e6dfce6d3708b9d
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2024-09-12 04:35:20 +0300

    addjust skills hover background color

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@
 
 const Card = (props) => {
 	return (
-		<div className="card w-28 h-28 rounded-xl border-transparent hover:bg-white/30 p-2 transition ease">
+		<div className="card w-28 h-28 rounded-xl border-transparent hover:bg-white/10 p-2 transition ease">
 			<img className="rounded-md" src={props.imgURL} alt={props.name}/>
 		</div>
 	)
