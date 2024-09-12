81985efca72c3af8f99893700bf5cf45bac81a88
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2024-09-12 04:35:47 +0300

    alt to skills

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@
 const Card = (props) => {
 	return (
 		<div className="card w-28 h-28 rounded-xl border-transparent hover:bg-white/10 p-2 transition ease">
-			<img className="rounded-md" src={props.imgURL} alt={props.name}/>
+			<img className="rounded-md" src={props.imgURL} alt="skill"/>
 		</div>
 	)
 }
