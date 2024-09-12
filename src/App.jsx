b56bd974ee7aeebc932b93577cb8a946f3e8b60f
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2024-09-12 20:46:10 +0300

    bg-edits-for-mobile

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Skills from "./section/Skills"
 
 function App() {
   return (
-    <div className=" bg-cover bg-fixed bg-[url(../public/background.svg)] bg-no-repeat max-[638px]:bg-[url(../public/background-portrait.svg)] max-[638px]:bg-center max-[638px]:bg-contain max-[638px]:bg-repeat max-[994px]:bg-[url(../public/background-portrait.svg)] max-[994px]:bg-cover max-[994px]:bg-repeat-y">
+    <div className=" bg-cover bg-fixed bg-[url(../public/background.svg)] bg-no-repeat max-[638px]:bg-[url(../public/background-portrait.svg)] max-[638px]:bg-center max-[638px]:bg-cover max-[638px]:bg-repeat max-[994px]:bg-[url(../public/background-portrait.svg)] max-[994px]:bg-cover max-[994px]:bg-repeat-y">
       <header className="h-16 flex justify-center items-center w-full absolute">
         <Navbar/>
       </header>
